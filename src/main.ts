a72ee4dd9935a9dca83e4c25bc42dd86c03f3650
drumath <2237k.drumath@gmail.com>
2024-10-27 05:17:51 +0900

    :sparkles: 試しにPlaneDetection

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import "./style.css";
-import { Engine, MeshBuilder, Scene } from "@babylonjs/core";
+import {
+  Engine,
+  type Mesh,
+  MeshBuilder,
+  PolygonMeshBuilder,
+  Scene,
+  Vector2,
+  WebXRFeatureName,
+  type WebXRPlaneDetector,
+} from "@babylonjs/core";
 
-const main = () => {
+const main = async () => {
   const renderCanvas =
     document.querySelector<HTMLCanvasElement>("#renderCanvas");
   if (!renderCanvas) {
@@ -14,6 +23,52 @@ const main = () => {
   scene.createDefaultCameraOrLight(true, true, true);
 
   MeshBuilder.CreateBox("box", { size: 0.5 });
+
+  const xr = await scene.createDefaultXRExperienceAsync({
+    uiOptions: {
+      sessionMode: "immersive-ar",
+      requiredFeatures: ["plane-detection"],
+    },
+  });
+
+  const featureManager = xr.baseExperience.featuresManager;
+  const planeDetector = featureManager.enableFeature(
+    WebXRFeatureName.PLANE_DETECTION,
+    "latest",
+  ) as WebXRPlaneDetector;
+
+  const planeMap = new Map<number, Mesh>();
+
+  planeDetector.onPlaneAddedObservable.add(
+    ({ id, polygonDefinition, transformationMatrix }) => {
+      const meshBuilder = new PolygonMeshBuilder(
+        "builder",
+        polygonDefinition.map((v) => new Vector2(v.x, v.z)),
+        scene,
+      );
+      const mesh = meshBuilder.build(false, 0.01);
+      transformationMatrix.decomposeToTransformNode(mesh);
+      planeMap.set(id, mesh);
+    },
+  );
+
+  planeDetector.onPlaneUpdatedObservable.add(
+    ({ id, polygonDefinition, transformationMatrix }) => {
+      const meshBuilder = new PolygonMeshBuilder(
+        "builder",
+        polygonDefinition.map((v) => new Vector2(v.x, v.z)),
+        scene,
+      );
+      const mesh = meshBuilder.build(false, 0.01);
+      transformationMatrix.decomposeToTransformNode(mesh);
+      planeMap.set(id, mesh);
+    },
+  );
+
+  planeDetector.onPlaneRemovedObservable.add(({ id }) => {
+    planeMap.get(id)?.dispose();
+    planeMap.delete(id);
+  });
 
   engine.runRenderLoop(() => scene.render());
   window.addEventListener("resize", () => engine.resize());
